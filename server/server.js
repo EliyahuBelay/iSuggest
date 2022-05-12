41147ab87d42905ed8a83749744477e7e87218c4
eliyahubelay0@gmail.com <eliyahubelay0@gmail.com>
2022-05-13 02:07:24 +0300

    create basic client side & connect it to heroku

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//----------------------------------
+//------------module that loads environment variables from a .env file into process.env----------------------
 require('dotenv').config();
 //----------------------------------
 
@@ -8,12 +8,29 @@ const express = require('express');
 //----------------------------------
 
 
+//-------------allowing the web page to be requested from another domain outside the domain from which the first resource was served---------------------
+const cors = require('cors');
+//----------------------------------
+
+
+//---------------------------------------------
+const path = require('path');
+//---------------------------------------------
+
+
+
+
 //---------connecting the database-------------------
 require('./DB/index')
 //----------------connecting the database------------
 
 
 const app = express();
+
+
+//-----------------------------
+app.use(cors());
+//-----------------------------
 
 
 //-------------------------------------
@@ -26,6 +43,13 @@ app.use(express.json());
 app.listen(process.env.PORT || 7000);
 //----------------------------------
 
-app.get('/',(request,response)=>{
-    response.send("server online");
-})
+
+
+//---------------------------------need to learn more about this------------------------------------------
+if (process.env.NODE_ENV === 'producton'){
+    app.use(express.static(path.join(__dirname,'../client/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../client/build','index.html'))
+    })
+}
+//---------------------------------------------------------------------------
